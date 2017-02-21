c88d871ebba580b94953679499b01ed8c2deda43
Xudong Huang <huangxu008@hotmail.com>
2017-02-21 14:43:08 +0800

    TokenId trait private

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
-#![feature(associated_consts)]
 #![feature(macro_reexport)]
 
 #[macro_reexport(TokenId)]
 extern crate token_id_plugins;
 
-pub trait TokenId {
-    const ID: u64;
-}
-
 #[macro_export]
 macro_rules! token_id {
     ($t:tt) => {{
-        use $crate::TokenId;
+        trait TokenId {
+            const ID: u64;
+        }
         #[derive(TokenId)]
         #[allow(non_camel_case_types)]
         struct $t;
